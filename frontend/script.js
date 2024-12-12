2198156dc5b7d76fe405e80e00289785f25b7088
wallace027dev <wallaceofc@hotmail.com>
2024-12-12 12:53:48 -0300

    estilizancao pronta

--- frontend/script.js
+++ frontend/script.js
@@ -3,13 +3,25 @@ $.ajax({
   method: "GET",
   success: function (data) {
     data.forEach(function (image) {
-      $("body").append(`
-        <div>
-          <h3>${image.title}</h3>
-          <img src="${image.url}" alt="${image.description}" />
-          <p>${image.description}</p>
+      $("#gallery").append(`
+        <div class="col-6 col-md-3">
+          <img 
+            src="${image.url}" 
+            alt="${image.description}" 
+            class="img-thumbnail img-click" 
+            data-bs-toggle="modal" 
+            data-bs-target="#imageModal"
+            data-caption="${image.description}">
         </div>
       `);
+    });
+
+    // Evento de clique para alterar imagem e legenda no modal
+    $(".img-click").on("click", function () {
+      const src = $(this).attr("src");
+      const caption = $(this).data("caption");
+      $("#modal-img").attr("src", src);
+      $("#modal-caption").text(caption);
     });
   },
   error: function () {
